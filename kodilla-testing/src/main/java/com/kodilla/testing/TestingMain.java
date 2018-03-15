bc4fd19be128732be664e176c8a2da31775c26f1
agatarauzer <agatarauzer@gmail.com>
2018-03-15 14:31:23 +0100

    exercise 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+        int a = 10;
+        int b = 20;
+
+        int addResult = calculator.add(a, b);
+        if (addResult == 30) {
+            System.out.println("Addition - correct result");
+        }
+        else {
+            System.out.println("Addition - wrong result");
+        }
+
+        int subtractResult = calculator.subtract(b, a);
+        if (subtractResult == 10) {
+            System.out.println("Subtraction - correct result");
+        }
+        else {
+            System.out.println("Subtraction - wrong result");
+        }
     }
 }
