ba9d4272d1cedd0e491208ecb52d81de1a006690
agatarauzer <agatarauzer@gmail.com>
2018-03-16 12:24:13 +0100

    exercise 6.3

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+
+        ArrayList<Integer> toRemoved = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            if (number % 2 != 0) {
+                toRemoved.add(number);
+            }
+        }
+
+        numbers.removeAll(toRemoved);
+        return numbers;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.testing.collection;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CollectionTestSuite {
+
+    @Before
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> testList = new ArrayList<>();
+
+        exterminator.exterminate(testList);
+
+        Assert.assertTrue(testList.isEmpty());
+    }
+
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> testList = new ArrayList<>();
+        for (int i = 1; i < 5; i++) {
+            testList.add(i);
+        }
+
+        exterminator.exterminate(testList);
+
+        Assert.assertTrue(testList.contains(2));
+        Assert.assertTrue(testList.contains(4));
+
+        Assert.assertFalse(testList.contains(1));
+        Assert.assertFalse(testList.contains(3));
+    }
+}
