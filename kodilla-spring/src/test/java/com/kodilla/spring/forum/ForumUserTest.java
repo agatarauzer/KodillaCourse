eaf4bfe04f57a7149d9e674d048f6a4ca598a86b
agatarauzer <agatarauzer@gmail.com>
2018-04-19 10:01:34 +0200

    exercise 10.1

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTest.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ForumUserTest {
+
+    @Test
+    public void testGetUsername() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser user = context.getBean(ForumUser.class);
+
+        //When
+        String name = user.getUsername();
+
+        //Then
+        Assert.assertEquals("John Smith", name);
+
+    }
+
+}
